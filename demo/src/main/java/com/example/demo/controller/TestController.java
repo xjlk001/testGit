69882a837bf4db4fc3257264478d592ccecbd886
huangmingtao <xjlk_001@163.com>
2025-09-20 11:50:06 +0800

    first commit

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import com.example.demo.Entity.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The type Test controller.
+ */
+@RestController
+public class TestController {
+
+    /**
+     * The User.
+     */
+    public final User user;
+
+    /**
+     * Instantiates a new Test controller.
+     *
+     * @param user the user
+     */
+    public TestController(User user) {
+        this.user = user;
+    }
+
+    /**
+     * Test string.
+     *
+     * @return the string
+     */
+    @RequestMapping("/test")
+    public String test() {
+        String name = user.getName();
+        String phone = user.getPhone();
+        String email = user.getEmail();
+        return "test";
+    }
+
+
+}
